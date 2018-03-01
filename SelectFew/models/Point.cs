85aa899bc306e524d8ea28ef2bfdb324d6acb4a1
Admin CodeValue <admin@codevalue.net>
2018-03-01 21:55:16 +0200

    Create map data structure

--- SelectFew/models/Point.cs
+++ SelectFew/models/Point.cs
@@ -16,5 +16,21 @@ namespace SelectFew.models
             X = x;
             Y = y;
         }
+
+        public override bool Equals(object obj)
+        {
+            var point = obj as Point;
+            return point != null &&
+                   X == point.X &&
+                   Y == point.Y;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1861411795;
+            hashCode = hashCode * -1521134295 + X.GetHashCode();
+            hashCode = hashCode * -1521134295 + Y.GetHashCode();
+            return hashCode;
+        }
     }
 }
